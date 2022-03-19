f47a5422795126422a8781e407571f63e7e94236
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-03-19 08:36:57 +0700

    feat : add auth redux

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import registration from "./registration";
 import forgotPassword from "./forgotPassword";
+import auth from "./auth"
 
 const rootReducer = combineReducers({
     registration,
-    forgotPassword
+    forgotPassword,
+    auth
 })
 
 export default rootReducer
