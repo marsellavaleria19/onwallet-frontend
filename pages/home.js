8b0033a1a652eb8653b531ae5479dd156c9f830e
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-05-24 11:05:23 +0700

    fix : fix balance in home page

--- pages/home.js
+++ pages/home.js
@@ -12,7 +12,6 @@ import BarChart from '../component/BarChart';
 import CList from '../component/CList';
 import Link from 'next/link';
 
-
 // import NavbarComponent from "../component/NavbarComponent";
 
 const Home = () =>{
@@ -24,14 +23,14 @@ const Home = () =>{
       <Layout>    
          <div className={home.balance}>
             <Row>
-               <Col xs="5" > 
+               <Col className='col-md' > 
                   <div className="ms-5 mt-4">
                      <div className="fs-4 text-secondary mb-2">Balance</div>
                      <div className="fs-1 fw-bold text-secondary mb-2">{auth.user!==null &&`Rp.${auth.balance!==null &&auth.balance.toLocaleString('id-ID')}`}</div>
                      <div className="fs-5 text-secondary">{auth.user!==null && auth.phone}</div>
                   </div>
                </Col>
-               <Col className="text-end me-3 me-md-3 me-xl-5">
+               <Col className="d-none d-sm-block col-md-4 col-xl-3 text-md-end me-3 me-md-3 me-xl-5">
                   <div className="mt-4">
                      <CButton className={`${home.button} btn-secondary text-primary fw-bold`} onClick={()=>route.push('/transaction/receiver')}><VscArrowUp className="me-2 fs-5 fw-bold"/>Transfer</CButton>
                   </div>
@@ -41,20 +40,28 @@ const Home = () =>{
                </Col>                        
             </Row>
          </div>
+         <Row className='d-sm-none mt-3'>
+            <Col>
+               <CButton className={`${home.button} btn-secondary text-primary fw-bold`} onClick={()=>route.push('/transaction/receiver')}><VscArrowUp className="me-2 fs-5 fw-bold"/>Transfer</CButton>
+            </Col>
+            <Col>
+               <CButton className={`${home.button} btn-secondary text-primary fw-bold`} onClick={()=>route.push('/transaction/topup')}><HiPlus className="me-2 fs-5 fw-bold"/>Top Up</CButton>
+            </Col>
+         </Row>
          <Row className="mt-3">
-            <Col md>
+            <Col md lg={6} className="mb-3">
                <div className={home.information}>
                   <Container>
                      <Row className="mt-4">
-                        <Col xs={6}>
-                           <div className="mt-3 ms-5">
-                              <VscArrowDown className="me-2  mb-2 fs-4 fw-bold text-success"/>
+                        <Col>
+                           <div className="mt-3 ms-md-5">
+                              <VscArrowDown className="me-2 mb-2 fs-4 fw-bold text-success"/>
                               <div className="fs-6 text-primary mb-2">Income</div>
                               <div className="fs-5 text-primary fw-bold">Rp. 1.200.000</div>
                            </div>
                         </Col>
-                        <Col xs={6}>
-                           <div className="mt-3 ms-3">
+                        <Col>
+                           <div className="mt-3 ms-md-3">
                               <VscArrowUp className="me-2 mb-2 fs-4 fw-bold text-danger"/>
                               <div className="fs-6 text-primary mb-2">Expanse</div>
                               <div className="fs-5 text-primary fw-bold">Rp. 1.156.000</div>
@@ -67,7 +74,7 @@ const Home = () =>{
                   </Container>
                </div>
             </Col>
-            <Col md>
+            <Col md lg>
                <div className={home.information}>
                   <div className="ms-4 me-4 mt-3">
                      <div className="d-flex justify-content-between mb-4">
