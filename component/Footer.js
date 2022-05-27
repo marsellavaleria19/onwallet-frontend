20b47e24b187e3a68e0938e983b4528c396e8c8e
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-05-27 13:32:07 +0700

    feat : update footer component

--- component/Footer.js
+++ component/Footer.js
@@ -3,11 +3,11 @@ import footer from '../styles/footer.module.scss';
 
 const Footer = ()=>{
    return(
-      <footer className={footer.footer}>
+      <footer className={footer.footerHome}>
          <Container>
             <div className="d-flex justify-content-between">
-               <div className="text-secondary">2020 Zwallet. All right reserved.</div>
-               <div className="text-secondary">+62 5637 8882 9901 contact@zwallet.com</div>
+               <div className="text-secondary">2020 On-Wallet. All right reserved.</div>
+               <div className="text-secondary">+62 5637 8882 9901 contact@on-wallet.com</div>
             </div>
          </Container>
       </footer>
