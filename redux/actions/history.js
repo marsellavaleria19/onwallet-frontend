b4536bbdb4c2b527ec9e1e0fffe3dfd125c9c720
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-05-27 13:20:34 +0700

    fix : fix history actions

--- redux/actions/history.js
+++ redux/actions/history.js
@@ -1,11 +1,11 @@
-import { default as axios } from 'axios'
-import qs from 'qs'
-import AxiosCostum from '../../helpers/AxiosCostum'
+import { default as axios } from 'axios';
+import qs from 'qs';
+import AxiosCostum from '../../helpers/AxiosCostum';
 
 
 export const getListHistory = (token) => {
-    return {
-        type: 'HISTORY',
-        payload: AxiosCostum(token).get('/transactions/history')
-    }
-}
+   return {
+      type: 'HISTORY',
+      payload: AxiosCostum(token).get('/transactions/history')
+   };
+};
